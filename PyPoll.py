a17c766ddc4545bb8a1bc6114c8f158e6304f9a4
GilianThompson <85901073+GilianThompson@users.noreply.github.com>
2021-06-30 16:01:16 -0400

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -23,10 +23,47 @@ with open(file_to_save, 'w') as txt_file:
     txt_file.write("---------------------------\n")
     txt_file.write("Arapahoe\nDenver\nJefferson")
 
+#initializing variables 
+total_votes = 0 
+candidate_options = [] #create empty array 
+candidate_votes= {} #create empty dictionary 
+winning_candidate = ""
+winning_count = 0 
+winning_percentage = 0 
+
 #open csv file to read, save as election_data
 with open(file_to_load) as election_data:
     #read file object with reader function
     file_reader = csv.reader(election_data)
+    #read header line (first row)
     headers = next(file_reader)
-    print(headers)
+    
+    for row in file_reader:
+        total_votes += 1 
+        candidate_name = row[2]
+        if candidate_name not in candidate_options: 
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0 
+        #increment vote count for the appropriate candidate     
+        candidate_votes[candidate_name] += 1 
+
+for candidate_name in candidate_options:
+    votes = candidate_votes[candidate_name] 
+    vote_percentage = float(votes)/float(total_votes) *100 
+    if (votes > winning_count) and (vote_percentage > winning_percentage): 
+        winning_count = votes 
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+
+winning_candidate_summary = (
+    f"------------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"------------------------------\n")
+
+print(winning_candidate_summary)
+
 
